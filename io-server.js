7044fa5d0c266579040b9b115d11e5c7edfeb7c3
Hideki Yamamoto <hideki.yamamoto@gmail.com>
2024-03-18 20:25:07 +0100

    inital

--- io-server.js
+++ io-server.js
@@ -109,7 +109,7 @@ function checkIfSignedIn(url) {
 
 // Initialize Admin SDK.
 admin.initializeApp({
-  credential: admin.credential.cert('serviceAccountKeys.json')
+  credential: admin.credential.cert('service-credentials.json')
 });
 // Support JSON-encoded bodies.
 app.use(bodyParser.json());
